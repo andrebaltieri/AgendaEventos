3e458392235aa4668d09620adcfbea3e26a2ff3a
Gabriel Nalin <n.gabriel@outlook.com.br>
2021-06-10 15:50:12 -0300

    Criada a pasta Data e Classe DbContext

---
+++ Data/DbContext.cs
@@ -0,0 +1,22 @@
+﻿using Agenda.Models;
+using AgendaEventos.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Agenda.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Entity> Entites { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
