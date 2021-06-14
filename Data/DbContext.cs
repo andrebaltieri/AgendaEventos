159b5d2f47d8eec8b4b97ff62c7cba20edfbbbaa
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-06-14 13:15:47 -0300

    Fix table name based on initial database script

--- Data/DbContext.cs
+++ Data/DbContext.cs
@@ -2,20 +2,22 @@
 using AgendaEventos.Models;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace Agenda.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options)
-            : base(options)
-        {
-        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Event> Events { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<User> Users { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Role>().ToTable(nameof(Role));
+        }
     }
 }
