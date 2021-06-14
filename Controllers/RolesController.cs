78ece65f212f38e677f4c0b3d332d15f37111177
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-06-14 14:53:29 -0300

    Fix versioning on api route

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Agenda.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class RolesController : ControllerBase
     {
