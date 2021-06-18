c85e3a90adafffef3b11641053ba609e618f6bcb
Jenifer Rodrigues <jen.mrodrigues@gmail.com>
2021-06-18 19:17:47 +0100

    Criacao do CRUD para usuarios.

--- Models/Entity.cs
+++ Models/Entity.cs
@@ -1,11 +1,16 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Agenda.Models
 {
     public abstract class Entity
     {
+        [Key]
         public int Id { get; set; }
+
         public DateTime CreatedDate { get; set; }
+
         public DateTime? LastUpdatedDate { get; set; }
+
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,7 @@
+using Agenda.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +21,9 @@ namespace Agenda
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
