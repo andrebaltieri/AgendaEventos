d76c11ac7a2faaa81c2698882d4c7db290897a3d
Jenifer Rodrigues <jen.mrodrigues@gmail.com>
2021-06-18 19:17:47 +0100

    Criação do CRUD para usuários.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,102 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Agenda.Data;
+using AgendaEventos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/v1/users")]
+public class UsersController : ControllerBase
+{
+    //Criar usuario
+    [HttpPost]
+    [Route("")]
+    public async Task<ActionResult<User>> Post(
+        [FromBody] User user,
+        [FromServices] DataContext context)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        try
+        {
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+        catch
+        {
+            return BadRequest(new { message = "Nao foi possivel incluir um novo usuario." });
+        }
+    }
+
+    //Consultar lista de usuarios
+    [HttpGet]
+    [Route("")]
+    public async Task<ActionResult<List<User>>> Get(
+        [FromServices] DataContext context)
+    {
+        var users = await context.Users.AsNoTracking().ToListAsync();
+        return Ok(users);
+    }
+
+    //Consultar usuario especifico por id
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<ActionResult<User>> GetById(
+        int id,
+        [FromServices] DataContext context)
+    {
+        var user = await context.Users.AsNoTracking().FirstOrDefaultAsync();
+
+        return Ok(user);
+    }
+
+    //Alterar usuario
+    [HttpPut]
+    [Route("{id:int}")]
+    public async Task<ActionResult<User>> Put(int id,
+    [FromBody] User user,
+    [FromServices] DataContext context)
+    {
+        if (user.Id != id) return NotFound(new { message = "Usuario nao encontrado" });
+
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        try
+        {
+            context.Entry<User>(user).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return user;
+
+        }
+        catch
+        {
+            return BadRequest(new { message = "Nao foi possivel alterar os dados do usuario." });
+        }
+    }
+
+    //Excluir usuario
+    [HttpDelete]
+    [Route("{id:int}")]
+    public async Task<ActionResult<User>> Delete(
+        int id,
+        [FromServices] DataContext context)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (user == null)
+            return NotFound(new { message = "Usuario nao encontrado." });
+
+        try
+        {
+            context.Users.Remove(user);
+            await context.SaveChangesAsync();
+            return Ok(user);
+        }
+        catch
+        {
+            return BadRequest(new { message = "Nao foi possivel excluir o usuario." });
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,13 +1,30 @@
 using System.Collections.Generic;
 using Agenda.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace AgendaEventos.Models
 {
     public class User : Entity
     {
+        [Required(ErrorMessage = "Esse campo e obrigatorio")]
+        [MaxLength(100, ErrorMessage = "Esse campo deve conter entre 3 e 100 caracteres.")]
+        [MinLength(3, ErrorMessage = "Esse campo deve conter entre 3 e 100 caracteres.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Esse campo e obrigatorio")]
+        [MaxLength(60, ErrorMessage = "Esse campo deve conter entre 3 e 60 caracteres.")]
+        [MinLength(3, ErrorMessage = "Esse campo deve conter entre 3 e 60 caracteres.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Esse campo e obrigatorio")]
+        [MaxLength(20, ErrorMessage = "Esse campo deve conter entre 3 e 20 caracteres.")]
+        [MinLength(3, ErrorMessage = "Esse campo deve conter entre 3 e 20 caracteres.")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Esse campo e obrigatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Tipo de usuario invalido")]
+        public int RoleId { get; set; }
+
         public IList<Role> Roles { get; set; }
     }
 }
