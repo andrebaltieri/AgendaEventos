d12217ef864e0c25cd9c1b0833a8a2f80141b2bf
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-06-19 15:02:42 -0300

    Fix route for controller

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Agenda.Controllers
 {
-    [Route("api/v1/[controller]")]
+    [Route("api/v1/roles")]
     [ApiController]
     public class RolesController : ControllerBase
     {
@@ -28,51 +28,7 @@ namespace Agenda.Controllers
                 await _context.Roles.AddAsync(model);
                 await _context.SaveChangesAsync();
 
-                return CreatedAtAction(nameof(GetRoleByIdAsync), new { id = model.Id }, model);
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine(ex.Message);
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Role>>> GetRolesAsync() => Ok(await _context.Roles.AsNoTracking().ToListAsync());
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<IEnumerable<Role>>> GetRoleByIdAsync(int id)
-        {
-            var role = await _context.Roles.FindAsync(id);
-
-            if (role is null)
-            {
-                return NotFound();
-            }
-
-            return Ok(role);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<ActionResult> UpdateRoleAsync(int id, Role model)
-        {
-            try
-            {
-                var role = await _context.Roles.FindAsync(id);
-
-                if (role is null)
-                {
-                    return NotFound();
-                }
-
-                role.Title = model.Title;
-                role.Description = model.Description;
-                role.LastUpdatedDate = DateTime.Now;
-
-                _context.Entry(role).State = EntityState.Modified;
-                await _context.SaveChangesAsync();
-
-                return NoContent();
+                return CreatedAtRoute(new { id = model.Id }, model);
             }
             catch (Exception ex)
             {
@@ -94,6 +50,50 @@ namespace Agenda.Controllers
                 }
 
                 _context.Roles.Remove(role);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Role>>> GetRoleByIdAsync(int id)
+        {
+            var role = await _context.Roles.FindAsync(id);
+
+            if (role is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(role);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Role>>> GetRolesAsync() => Ok(await _context.Roles.AsNoTracking().ToListAsync());
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateRoleAsync(int id, Role model)
+        {
+            try
+            {
+                var role = await _context.Roles.FindAsync(id);
+
+                if (role is null)
+                {
+                    return NotFound();
+                }
+
+                role.Title = model.Title;
+                role.Description = model.Description;
+                role.LastUpdatedDate = DateTime.Now;
+
+                _context.Entry(role).State = EntityState.Modified;
                 await _context.SaveChangesAsync();
 
                 return NoContent();
