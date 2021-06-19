a46b3a2f652dedf3cf0bf5b7ffb856bb300f6cf8
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-06-19 15:01:57 -0300

    Fix mapping for date properties

--- Models/Entity.cs
+++ Models/Entity.cs
@@ -1,11 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Agenda.Models
 {
     public abstract class Entity
     {
-        public int Id { get; set; }
+        [Column(TypeName = nameof(DateTime))]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime CreatedDate { get; set; }
+
+        public int Id { get; set; }
+
+        [Column(TypeName = nameof(DateTime))]
         public DateTime? LastUpdatedDate { get; set; }
     }
 }
