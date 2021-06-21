06f989acbb528f1d83bdc23279e65a6ead30b0d4
Paulo Barbosa <paulo.barbosa@criainovacao.com.br>
2021-06-21 20:53:57 -0300

    CRUD Events

---
+++ Controllers/EventController.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Threading.Tasks;
+using Agenda.Data;
+using Agenda.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agenda.Controllers
+{
+    [Route("api/v1/events")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EventController(DataContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        [Route("")]
+        public async Task<List<Event>> Get()
+        {
+            var events = await _context.Events
+                .Include(e => e.Category)
+                .AsNoTracking()
+                .ToListAsync();
+            return events;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<Event> GetById(int id)
+        {
+            var events = await _context.Events
+                .Include(e => e.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return events;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Event>> Post([FromBody] Event model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(model);
+            }
+
+            try
+            {
+                _context.Events.Add(model);
+                await _context.SaveChangesAsync();
+                return model;
+            }
+            catch (Exception)
+            {
+                return BadRequest(new {message = "Não foi possível criar o evento"});
+            }
+        }
+
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Event>> Update(int id, [FromBody] Event model)
+        {
+            if (id != model.Id)
+                return NotFound(new {message = "Evento não encontrado"});
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                _context.Entry<Event>(model).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return model;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return BadRequest(new {message = "Não foi possível atualizar o evento"});
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Event>> Delete(int id)
+        {
+            var events = await _context.Events
+                .Include(e => e.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (events == null)
+            {
+                return NotFound(new {message = "Evento não encontrado"});
+            }
+            
+            try
+            {
+                _context.Remove(events);
+                await _context.SaveChangesAsync();
+                return events;
+            }
+            catch (Exception)
+            {
+                return BadRequest(new {message = "Não foi possível remover o evento"});
+            }
+        }
+
+       
+    }
+}
--- Models/Event.cs
+++ Models/Event.cs
@@ -1,20 +1,57 @@
+using System.ComponentModel.DataAnnotations;
 using AgendaEventos.Models;
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Agenda.Models
 {
-    public class Event : Entity
-    {
-        public string Title { get; set; }
-        public User Speaker { get; set; }
-        public string Description { get; set; }
-        public DateTime StartDate { get; set; }
-        public int DurationInMinutes { get; set; }
-        public DateTime EnrollmentDeadlineDate { get; set; }
-        public string UrlSegment { get; set; }
-        public string BannerUrl { get; set; }
-        public bool Active { get; set; }
-        public User Organizer { get; set; }
-        public Category Category { get; set; }
-    }
+  [Table(nameof(Event))]
+  public class Event : Entity
+  {
+    [Required(ErrorMessage = "Este campo é obrigatório")]
+    [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+    [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "Este campo é obrigatório")]
+    [Range(1, int.MaxValue, ErrorMessage = "Tipo de usuario inválido")]
+    public int SpeakerId { get; set; }
+    public User Speaker { get; set; }
+
+    [Required(ErrorMessage = "Este campo é obrigatório")]
+    public string Description { get; set; }
+
+    [Required(ErrorMessage = "Este campo é obrigatório")]
+    [Column(TypeName = nameof(DateTime))]
+    public DateTime StartDate { get; set; }
+
+    [Required(ErrorMessage = "Este campo é obrigatório")]
+    [Range(1,60, ErrorMessage = "Este campo deve conter entre 1 e 60 minutos")]
+    public int DurationInMinutes { get; set; }
+    
+    [Required(ErrorMessage = "Este campo é obrigatório")]
+    [Column(TypeName = nameof(DateTime))]
+    public DateTime EnrollmentDeadlineDate { get; set; }
+    
+    [Required(ErrorMessage = "Este campo é obrigatório")]
+    [MaxLength(1024, ErrorMessage = "Este campo deve conter no máximo 1024 caracteres")]
+    public string UrlSegment { get; set; }
+    
+    [Required(ErrorMessage = "Este campo é obrigatório")]
+    [MaxLength(2000, ErrorMessage = "Este campo deve conter no máximo 2000 caracteres")]
+    public string BannerUrl { get; set; }
+    
+    [Required(ErrorMessage = "Este campo é obrigatório")]
+    public bool Active { get; set; }
+    
+    [Required(ErrorMessage = "Este campo é obrigatório")]
+    [Range(1, int.MaxValue, ErrorMessage = "Tipo de usuario inválido")]
+    public int OrganizerId { get; set; }
+    public User Organizer { get; set; }
+    
+    [Required(ErrorMessage = "Este campo é obrigatório")]
+    [Range(1, int.MaxValue, ErrorMessage = "Categoria inválida")]
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+  }
 }
