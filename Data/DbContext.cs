8aee5db9780e60923ba013df590ed69a997583fb
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-06-23 23:02:19 -0300

    Define mapeamento para a tabela de relação UserRoles

--- Data/DbContext.cs
+++ Data/DbContext.cs
@@ -2,6 +2,7 @@
 using AgendaEventos.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 
 namespace Agenda.Data
 {
@@ -15,5 +16,27 @@ namespace Agenda.Data
         public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.LogTo(Console.WriteLine);
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<User>()
+                .HasMany(u => u.Roles)
+                .WithMany(r => r.Users)
+                .UsingEntity<Dictionary<string, object>>(
+                    "UserRoles",
+                    ur => ur
+                    .HasOne<Role>()
+                    .WithMany()
+                    .HasForeignKey("RoleId")
+                    .HasConstraintName("FK_UserRoles_Role_RoleId")
+                    .OnDelete(DeleteBehavior.Cascade),
+                    ur => ur
+                    .HasOne<User>()
+                    .WithMany()
+                    .HasForeignKey("UserId")
+                    .HasConstraintName("FK_UserRoles_User_UserId")
+                    .OnDelete(DeleteBehavior.ClientCascade));
+        }
     }
 }
